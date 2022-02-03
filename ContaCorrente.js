dde6b9378b01de947d526b294020a26ff345210f
Ju Andrade <juliana.andraden07@gmail.com>
2022-02-03 20:33:46 -0300

    Conta Poupanca e atualizacao

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,10 +2,7 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente {
     static numeroDeContas = 0;
-    agencia;
-    _cliente;
-    _saldo = 0;
-
+   
     set cliente(novoValor) {
         if (novoValor instanceof Cliente) {
             this._cliente = novoValor;
@@ -23,6 +20,7 @@ export class ContaCorrente {
     constructor(agencia, cliente) {
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
     }
 
