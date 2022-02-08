afe28192117be501515c3148daf6d1581d628ea1
Ju Andrade <juliana.andraden07@gmail.com>
2022-02-08 11:02:33 -0300

    Aula 2

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente1 = new Cliente("Ricardo", 111645614545);
 
-const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
 contaCorrenteRicardo.depositar(500);
 contaCorrenteRicardo.sacar(100);
 
-const ContaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+contaPoupanca.sacar(10);
 
-console.log(ContaPoupanca);
+console.log(contaPoupanca);
 console.log(contaCorrenteRicardo);
